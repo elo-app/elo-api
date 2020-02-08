ec4b6367e3a0ca15ff470e2b359fa8c55c64263e
Lucas Phillips <lucas.couto.phillips@gmail.com>
2020-02-08 16:17:27 -0500

    add pymongo

--- api.py
+++ api.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask_cors import CORS
-from flask_restful import Resource, Api
+from flask_restful import Api, Resource
+from pymongo import MongoClient
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
+
+client = MongoClient()
+
+print(client)
 
 
 class HelloWorld(Resource):
